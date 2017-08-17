8c74130cdff3995ecd780ee0b9be6ffc972e4c32
Andrew Duckworth <grillopress@gmail.com>
2017-08-17 21:13:14 +0000

    add styles

--- functions.php
+++ functions.php
@@ -9,17 +9,17 @@ function sth_scripts() {
   // 
   // Comment out to remove
   
-    $parent_style = 'WP_NHS';
+ //   $parent_style = 'WP_NHS';
 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+ //   wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+ //   wp_enqueue_style( 'child-style',
+ //       get_stylesheet_directory_uri() . '/style.css',
+ //       array( $parent_style ),
+ //       wp_get_theme()->get('Version')
+ //   );
   
  // Include only child
  // 
- // wp_enqueue_style( 'sth-style', get_stylesheet_uri() );
+ wp_enqueue_style( 'sth-style', get_stylesheet_uri() );
 
 }
